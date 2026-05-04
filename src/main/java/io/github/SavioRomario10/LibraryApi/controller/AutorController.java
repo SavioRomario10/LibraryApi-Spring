b50f7d857c39655199ef6aa1c91e76e014900537
Sávio Romário <savioromario10@gmail.com>
2026-05-04 16:33:40 -0300

    criando o primeiro DTO

--- src/main/java/io/github/SavioRomario10/LibraryApi/controller/AutorController.java
+++ src/main/java/io/github/SavioRomario10/LibraryApi/controller/AutorController.java
@@ -1,10 +1,20 @@
 package io.github.SavioRomario10.LibraryApi.controller;
 
+import io.github.SavioRomario10.LibraryApi.controller.dto.AutorDTO;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/autores")
 public class AutorController {
 
+  @PostMapping
+  public ResponseEntity salvar(@RequestBody AutorDTO autor){
+    return new ResponseEntity("Autor salvo com sucesso!" + autor, HttpStatus.CREATED);
+  }
 }
---
+++ src/main/java/io/github/SavioRomario10/LibraryApi/controller/dto/AutorDTO.java
@@ -0,0 +1,7 @@
+package io.github.SavioRomario10.LibraryApi.controller.dto;
+
+import java.time.LocalDate;
+
+public record AutorDTO(String nome, LocalDate dataNascimento, String nacionalidade) {
+
+}
