09f4707137cb713acfa7d2ade0e23b580b735e25
Sávio Romário <savioromario10@gmail.com>
2026-05-07 17:27:07 -0300

    aplicando validações para api livros

--- src/main/java/io/github/SavioRomario10/LibraryApi/controller/LivroController.java
+++ src/main/java/io/github/SavioRomario10/LibraryApi/controller/LivroController.java
@@ -2,8 +2,18 @@ package io.github.SavioRomario10.LibraryApi.controller;
 
 import io.github.SavioRomario10.LibraryApi.services.LivroService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import jakarta.validation.Valid;
+
+import io.github.SavioRomario10.LibraryApi.controller.dto.CadastroLivroDTO;
+import io.github.SavioRomario10.LibraryApi.exception.RegistroDuplicadoException;
+import io.github.SavioRomario10.LibraryApi.controller.dto.ErroResposta;
 
 @RestController
 @RequestMapping("/livros")
@@ -11,4 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
   private final LivroService service;
+
+  @PostMapping
+  public ResponseEntity<Object> salvar(
+    @RequestBody @Valid CadastroLivroDTO dto){
+
+      try{
+        return ResponseEntity.ok(dto);
+      }
+      catch(RegistroDuplicadoException e){
+        var erroDTO = ErroResposta.conflito(e.getMessage());
+        return ResponseEntity.status(erroDTO.Status()).body(erroDTO);
+      }
+  }
 }
--- src/main/java/io/github/SavioRomario10/LibraryApi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/SavioRomario10/LibraryApi/controller/dto/CadastroLivroDTO.java
@@ -1,17 +1,29 @@
 package io.github.SavioRomario10.LibraryApi.controller.dto;
 
 import java.util.UUID;
+
+import org.hibernate.validator.constraints.ISBN;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import io.github.SavioRomario10.LibraryApi.model.enums.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 public record CadastroLivroDTO(
+  @ISBN(message = "ISBN inválido")
+  @NotBlank(message = "ISBN é obrigatório")
   String isbn,
+  @NotBlank(message = "Título é obrigatório")
   String titulo,
+  @NotNull(message = "Data de publicação é obrigatória")
+  @Past(message = "Data inválida")
   LocalDate dataPublicacao,
   GeneroLivro genero,
   BigDecimal preco,
+  @NotNull(message = "Autor é obrigatório")
   UUID idAutor
 ) {
 
