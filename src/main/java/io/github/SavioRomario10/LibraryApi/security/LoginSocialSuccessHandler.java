07998d5b53d46ed8b4e24fba271ce83df016fa18
Sávio Romário <savioromario10@gmail.com>
2026-05-20 15:38:58 -0300

    finalizando login social, criando usuario automatico

--- src/main/java/io/github/SavioRomario10/LibraryApi/security/LoginSocialSuccessHandler.java
+++ src/main/java/io/github/SavioRomario10/LibraryApi/security/LoginSocialSuccessHandler.java
@@ -1,6 +1,7 @@
 package io.github.SavioRomario10.LibraryApi.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,6 +9,8 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import io.github.SavioRomario10.LibraryApi.model.Usuario;
 import io.github.SavioRomario10.LibraryApi.services.UsuarioService;
@@ -19,6 +22,8 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler{
+
+  private static final String SENHA_PADRAO = "123";
   
   private final UsuarioService service;
 
@@ -37,10 +42,29 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
 
     Usuario usuario = service.obterPorEmail(email);
 
+    if(usuario == null){
+      usuario = cadastrarUsuario(email);
+    }
+
     authentication = new CustomAuthentication(usuario);
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
 
     super.onAuthenticationSuccess(request, response, authentication);
+  }
+
+  private Usuario cadastrarUsuario(String email){
+    Usuario usuario = new Usuario();
+    usuario.setEmail(email);
+    usuario.setLogin(obterLoginPorEmail(email));
+    usuario.setSenha(SENHA_PADRAO);
+    usuario.setRoles(List.of("OPERADOR"));
+
+    service.salvar(usuario);
+    return usuario;
+  }
+
+  private String obterLoginPorEmail(String email){
+    return email.substring(0, email.indexOf("@"));
   }
 }  
